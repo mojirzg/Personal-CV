9527d8577c56592340da938f154136a7f8308665
mojirzg <moji.razaghi@gmail.com>
2023-09-20 04:35:44 +0330

    Chore: added portfolio

--- src/components/ui/portfolio/Portfolio.tsx
+++ src/components/ui/portfolio/Portfolio.tsx
@@ -3,33 +3,38 @@ import React, { FunctionComponent } from "react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
+import Styles from "./Portfolio.module.scss";
 
 interface Props {}
 
 const items = [
   {
-    image: "/images/me.jpg",
-    name: "name",
-    description: "description",
-    link: "/",
+    image: "/images/portfolio/zhin/logo.jpg",
+    background: "/images/portfolio/zhin/home.jpg",
+    name: "Zhin Tour",
+    description: "Next.js Express.js",
+    link: "https://zhintour.com",
   },
   {
-    image: "/images/me.jpg",
-    name: "name",
-    description: "description",
-    link: "/",
+    image: "/images/portfolio/kss/logo.jpg",
+    background: "/images/portfolio/kss/home.jpg",
+    name: "Kizeshe Sabze Shomal",
+    description: "Next.js Express.js",
+    link: "https://kssapp.ir",
   },
   {
-    image: "/images/me.jpg",
-    name: "name",
-    description: "description",
-    link: "/",
+    image: "/images/portfolio/almas/logo.jpg",
+    background: "/images/portfolio/almas/home.jpg",
+    name: "Almas Karimkhan",
+    description: "Next.js",
+    link: "https://almaskarimkhan.com",
   },
   {
-    image: "/images/me.jpg",
-    name: "name",
-    description: "description",
-    link: "/",
+    image: "/images/portfolio/chainwall/logo.jpg",
+    background: "/images/portfolio/chainwall/home.jpg",
+    name: "Chainwall",
+    description: "Next.js Nest.js",
+    link: "https://chainwall.io",
   },
 ];
 
@@ -40,7 +45,7 @@ export const Portfolio: FunctionComponent<Props> = () => {
       <p className="sticky items-center justify-center text-center portfolio-text md:top-7 md:flex">
         {t("portfolio")}
       </p>
-      <div className="w-full gap-6 space-y-6 md:columns-2">
+      <div className="w-full gap-4 space-y-4 columns-2">
         {items.map((item, index) => (
           <div
             className="relative flex flex-col w-full overflow-hidden transition-all duration-100 rounded-md hover:scale-105 bg-background-card"
@@ -48,14 +53,26 @@ export const Portfolio: FunctionComponent<Props> = () => {
           >
             <Link href={item.link}>
               <div
-                className="relative flex-1 w-full aspect-[2/1] bg-background-card"
+                className={`${Styles.imageContainer} relative flex-1 w-full aspect-[16/9] bg-background-card`}
                 key={index}
               >
-                <Image fill src={item.image} className="object-cover" alt="" />
+                <Image
+                  fill
+                  src={item.background}
+                  className={`${Styles.background}`}
+                  alt=""
+                />
+
+                <Image
+                  fill
+                  src={item.image}
+                  className={`${Styles.thumbnail} object-cover`}
+                  alt=""
+                />
               </div>
               <p className="px-4 pt-2 text-lg font-bold">{item.name}</p>
               <p className="px-4 py-0 pb-4 text-lg font-bold text-content-secondary">
-                {item.name}
+                {item.description}
               </p>
             </Link>
           </div>
