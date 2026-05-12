bae46c26cd5ca70a1ab94e7e9f8302ea806d01f3
mojtaba <mojtaba@shuwela.com>
2026-05-12 17:52:29 +0300

    updated

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Hero, NavMenu, Portfolio } from '@/components';
 import Contact from '@/components/ui/contact/Contact';
 import Variants from '@/components/common/nav/NavMenu';
